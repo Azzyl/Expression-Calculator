8dfe5cbc45e8dde9c94790fbc2ff0f860a4c32ef
Азат Бельгибаев <satellite@MacBook-Air-Azat.local>
2018-09-09 16:25:36 +0300

    modified Readme.md

--- src/test/kotlin/ParserTest.kt
+++ src/test/kotlin/ParserTest.kt
@@ -2,15 +2,47 @@ import junit.framework.TestCase
 
 class ParserTest : TestCase() {
 
-    fun testParse() {}
+    val objectUnderTest = Parser()
 
-    fun testParseRelation() {}
+    fun testParse() {
+        val input = "(2+2*2)"
+        val correctValue = 6L
+        val testValue = objectUnderTest.parse(input).value
+        assertTrue(correctValue == testValue)
+    }
 
-    fun testParseTerm() {}
+    fun testParseRelation() {
+        val input = "1<2"
+        val correctValue = 1L
+        val testValue = objectUnderTest.parseRelation(input).value
+        assertTrue(correctValue == testValue)
+    }
 
-    fun testParseFactor() {}
+    fun testParseTerm() {
+        val input = "12-10+4"
+        val correctValue = 6L
+        val testValue = objectUnderTest.parseTerm(input).value
+        assertTrue(correctValue == testValue)
+    }
 
-    fun testParsePrimary() {}
+    fun testParseFactor() {
+        val input = "1*2*3*4*5*0"
+        val correctValue = 0L
+        val testValue = objectUnderTest.parseFactor(input).value
+        assertTrue(correctValue == testValue)
+    }
 
-    fun testParseInteger() {}
+    fun testParsePrimary() {
+        val input = "(3)"
+        val correctValue = 3L
+        val testValue = objectUnderTest.parsePrimary(input).value
+        assertTrue(correctValue == testValue)
+    }
+
+    fun testParseInteger() {
+        val input = "123"
+        val correctValue = 123L
+        val testValue = objectUnderTest.parse(input).value
+        assertTrue(correctValue == testValue)
+    }
 }
