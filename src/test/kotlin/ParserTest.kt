e3ec0d88cfd269c38cd77f913e383074ff2696cd
Азат Бельгибаев <satellite@MacBook-Air-Azat.local>
2018-09-09 16:15:22 +0300

    Added testes

---
+++ src/test/kotlin/ParserTest.kt
@@ -0,0 +1,16 @@
+import junit.framework.TestCase
+
+class ParserTest : TestCase() {
+
+    fun testParse() {}
+
+    fun testParseRelation() {}
+
+    fun testParseTerm() {}
+
+    fun testParseFactor() {}
+
+    fun testParsePrimary() {}
+
+    fun testParseInteger() {}
+}
